1a8f29a7aae8db61a2a4921d9f7a3b1c7191fc1f
s224027778 <92920346+s224027778@users.noreply.github.com>
2024-08-05 16:56:15 +0200

    Update housePrices.py

--- housePrices.py
+++ housePrices.py
@@ -3,18 +3,14 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
 
-# Sample data
 X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)  # Feature (e.g., house size)
 y = np.array([150, 200, 250, 300, 350])  # Target (e.g., house price)
 
-# Create and train the model
 model = LinearRegression()
 model.fit(X, y)
 
-# Predict
 y_pred = model.predict(X)
 
-# Plot results
 plt.scatter(X, y, color='blue')  # Actual data
 plt.plot(X, y_pred, color='red')  # Predicted data
 plt.xlabel('House Size')
